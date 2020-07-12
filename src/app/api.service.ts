1edba1f887acd20634c94a724428a11e2b77bfc0
MinhHuynh <minhhuynhjm@gmail.com>
2020-07-12 20:52:07 +0700

    add AWS upload file

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,30 +1,33 @@
-import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap, map } from 'rxjs/operators';
-import { Post, PostList } from '../models/Posts';
+import { Injectable } from "@angular/core";
+import { Observable, of, throwError } from "rxjs";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpRequest,
+} from "@angular/common/http";
+import { catchError, tap, map, retry } from "rxjs/operators";
+import { Post, PostList } from "../models/Posts";
 const httpOptions = {
-  headers: new HttpHeaders()
+  headers: new HttpHeaders(),
 };
-httpOptions.headers.append('Content-Type', 'application/json');
-httpOptions.headers.append('Content-Type', 'multipart/form-data');
-const apiUrl = 'http://technews.com/api/';//http://localhost:50718/
-//const apiUrl = 'http://localhost:50718/api/';
-const apiPosts = apiUrl + 'posts';
-const apiPostByCategory = apiPosts + '/Categories';
-const apiPaging = apiPosts + '/Category';
-const apiTest = 'http://192.168.66.36/api/export';
-//const apiTest = 'http://192.168.50.65/api/export';
+httpOptions.headers.append("Content-Type", "application/json");
+httpOptions.headers.append("Content-Type", "multipart/form-data");
+const apiUrl = "http://technews.com/api/";
+const apiPosts = apiUrl + "posts";
+const apiPostByCategory = apiPosts + "/Categories";
+const apiPaging = apiPosts + "/Category";
+const apiTest = "http://192.168.66.36/api/export";
+const apiAwsPut =
+  "https://learningjm.s3.ap-southeast-1.amazonaws.com/hello/face.jpg?X-Amz-Expires=600&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAYQ6VWBCNX3G75HFG/20200712/ap-southeast-1/s3/aws4_request&X-Amz-Date=20200712T134930Z&X-Amz-SignedHeaders=host&X-Amz-Signature=492c5e0601d5ab87d273d3019aed3dd161534e524ea457a2eaf7f9ef71355044";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  private handleError<T>(operation = 'operation', result?: T) {
+  private handleError<T>(operation = "operation", result?: T) {
     return (error: any): Observable<T> => {
-
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
@@ -34,43 +37,55 @@ export class ApiService {
   }
 
   getListPosts(): Observable<PostList[]> {
-    return this.http.get<PostList[]>(apiPosts)
-      .pipe(
-        tap(_ => console.log('fetch list posts')),
-        catchError(this.handleError('getPostLists', []))
-      );
+    return this.http.get<PostList[]>(apiPosts).pipe(
+      tap((_) => console.log("fetch list posts")),
+      catchError(this.handleError("getPostLists", []))
+    );
   }
 
   getListPostByCategory(id: number): Observable<PostList[]> {
     const url = `${apiPostByCategory}/${id}`;
-    return this.http.get<PostList[]>(url)
-      .pipe(
-        tap(_ => console.log('fetch list posts')),
-        catchError(this.handleError('getPostLists', []))
-      );
+    return this.http.get<PostList[]>(url).pipe(
+      tap((_) => console.log("fetch list posts")),
+      catchError(this.handleError("getPostLists", []))
+    );
   }
 
   getPagination(id: number, page: number): Observable<any> {
     const url = `${apiPaging}/${id}/${page}`;
-    return this.http.get<any>(url)
-      .pipe(
-        tap(_ => console.log('fetch list posts')),
-        catchError(this.handleError('getPostLists', []))
-      );
+    return this.http.get<any>(url).pipe(
+      tap((_) => console.log("fetch list posts")),
+      catchError(this.handleError("getPostLists", []))
+    );
   }
 
   getPost(id: number): Observable<Post> {
     const url = `${apiPosts}/${id}`;
     return this.http.get<Post>(url).pipe(
-      tap(_ => console.log(`fetched post id=${id}`)),
+      tap((_) => console.log(`fetched post id=${id}`)),
       catchError(this.handleError<Post>(`getPost id=${id}`))
     );
   }
 
+  uploadS3(file: File) {
+    if (!file) {
+      return;
+    }
+    const req = new HttpRequest("PUT", apiAwsPut, file, {
+      reportProgress: true,
+    });
+
+    // The `HttpClient.request` API produces a raw event stream
+    // which includes start (sent), progress, and response events.
+    return this.http.request(req).pipe();
+  }
+
   addPost(post): Observable<any> {
-    return this.http.post<any>(apiTest, post, httpOptions).pipe(
+    return this.http
+      .put<any>(apiAwsPut, post, httpOptions)
+      .pipe
       // tap((post: Post) => console.log('added post')),
       // catchError(this.handleError<Post>('error add Post'))
-    );
+      ();
   }
 }
--- src/app/post-add/post-add.component.ts
+++ src/app/post-add/post-add.component.ts
@@ -1,81 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
-import { Post } from 'src/models/Posts';
-import { Router } from '@angular/router';
-import { ApiService } from '../api.service';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators, NgForm } from "@angular/forms";
+import { Post } from "src/models/Posts";
+import { Router } from "@angular/router";
+import { ApiService } from "../api.service";
 
 @Component({
-    selector: 'app-post-add',
-    templateUrl: './post-add.component.html',
-    styleUrls: ['./post-add.component.scss']
+  selector: "app-post-add",
+  templateUrl: "./post-add.component.html",
+  styleUrls: ["./post-add.component.scss"],
 })
 export class PostAddComponent implements OnInit {
+  postForm: FormGroup;
+  post: Post;
+  isLoadingResults = false;
+  image: File = null;
 
-    postForm: FormGroup;
-    post: Post;
-    isLoadingResults = false;
-    image: File = null;
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private formBuilder: FormBuilder
+  ) {}
 
-    constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
+  ngOnInit() {
+    this.postForm = this.formBuilder.group({
+      postTitle: ["", Validators.required],
+      postContent: ["", Validators.required],
+      postDecription: ["", Validators.required],
+      categoryId: ["", Validators.required],
+      postStatus: ["", Validators.required],
+      tag: ["", Validators.required],
+    });
+  }
 
-    }
+  // onFormSubmit(form: NgForm) {
+  //   this.isLoadingResults = true;
+  //   console.log('ddd ', form);
+  //   this.api.addPost(form)
+  //     .subscribe(res => {
+  //       console.log('res: ', res);
+  //       let id = res;
+  //       this.isLoadingResults = false;
+  //       // this.router.navigate(['/posts']);
+  //     }, (err) => {
+  //       console.log(err);
+  //       this.isLoadingResults = false;
+  //     });
+  // }
 
+  converFormGroupToFormData(formData: FormData) {
+    // formData.append('PostTitle', this.postForm.get('postTitle').value);
+    formData.append("excel", this.image);
+    // formData.append('PostContent', this.postForm.get('postContent').value);
+    // formData.append('PostDecription', this.postForm.get('postDecription').value);
+    // formData.append('CategoryId', this.postForm.get('categoryId').value);
+    // formData.append('PostStatus', this.postForm.get('postStatus').value);
+    // formData.append('Tag', this.postForm.get('tag').value);
+  }
 
-    ngOnInit() {
-        this.postForm = this.formBuilder.group({
-            postTitle: ['', Validators.required],
-            postContent: ['', Validators.required],
-            postDecription: ['', Validators.required],
-            categoryId: ['', Validators.required],
-            postStatus: ['', Validators.required],
-            tag: ['', Validators.required]
-        });
-    }
-
-    // onFormSubmit(form: NgForm) {
-    //   this.isLoadingResults = true;
-    //   console.log('ddd ', form);
-    //   this.api.addPost(form)
-    //     .subscribe(res => {
-    //       console.log('res: ', res);
-    //       let id = res;
-    //       this.isLoadingResults = false;
-    //       // this.router.navigate(['/posts']);
-    //     }, (err) => {
-    //       console.log(err);
-    //       this.isLoadingResults = false;
-    //     });
-    // }
-
-    converFormGroupToFormData(formData: FormData){
-        formData.append('PostTitle', this.postForm.get('postTitle').value);
-        formData.append('excel', this.image);
-        formData.append('PostContent', this.postForm.get('postContent').value);
-        formData.append('PostDecription', this.postForm.get('postDecription').value);
-        formData.append('CategoryId', this.postForm.get('categoryId').value);
-        formData.append('PostStatus', this.postForm.get('postStatus').value);
-        formData.append('Tag', this.postForm.get('tag').value);
-    }
-
-
-    // Non Function Formdata
-    onFormSubmit() {
-        const formData = new FormData();
-        this.converFormGroupToFormData(formData);
-
-        this.isLoadingResults = true;
-        this.api.addPost(formData)
-            .subscribe(res => {
-                console.log('res: ', res);
-                this.isLoadingResults = false;
-                // this.router.navigate(['/posts']);
-            }, (err) => {
-                console.log(err);
-                this.isLoadingResults = false;
-            });
-    }
-    handleFileInput(files: FileList) {
-        this.image = files[0];
-    }
-
+  // Non Function Formdata
+  onFormSubmit() {
+    // const formData = new FormData();
+    // this.converFormGroupToFormData(formData);
+    this.api.uploadS3(this.image).subscribe(
+      (res) => {
+        console.log("res: ", res);
+        this.isLoadingResults = false;
+        // this.router.navigate(['/posts']);
+      },
+      (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+    );
+    this.isLoadingResults = true;
+    // this.api.addPost(formData).subscribe(
+    //   (res) => {
+    //     console.log("res: ", res);
+    //     this.isLoadingResults = false;
+    //     // this.router.navigate(['/posts']);
+    //   },
+    //   (err) => {
+    //     console.log(err);
+    //     this.isLoadingResults = false;
+    //   }
+    // );
+  }
+  handleFileInput(files: FileList) {
+    this.image = files[0];
+  }
 }
